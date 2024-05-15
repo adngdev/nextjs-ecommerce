39a1cb65b074a3d252eaf4039f9472324886c682
JamieNguyen <jamie@specspace.com>
2024-05-15 13:50:10 +1000

    adjust toast configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,34 @@
-import { ReactNode } from 'react';
-
 import { Inter } from 'next/font/google';
-
-import './globals.css';
+import { ReactNode } from 'react';
 import { Toaster } from 'sonner';
 
-const inter = Inter({ subsets: ["latin"] });
+import './globals.css';
+
+import Providers from '@/components/Providers';
+import Navbar from '@/components/Navbar';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
     return (
         <html lang="en">
             <body className={inter.className}>
-                <Toaster />
-                {children}
+                <Providers>
+                    <Navbar />
+                    <Toaster
+                        position={`top-center`}
+                        toastOptions={{
+                            unstyled: true,
+                            classNames: {
+                                error: 'bg-red-400',
+                                success: 'text-green-400',
+                                warning: 'text-yellow-400',
+                                info: 'bg-blue-400',
+                            }
+                        }}
+                    />
+                    {children}
+                </Providers>
             </body>
         </html>
     );
