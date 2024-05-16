b056ad99c293e1ee96e3145b6f6b63a598bb2085
Jamie <adungg2509@gmail.com>
2024-05-16 22:15:37 +1000

    modified several components related to authentication

--- src/app/(auth)/sign-in/action.ts
+++ src/app/(auth)/sign-in/action.ts
@@ -1,14 +1,22 @@
 'use server'
 
-import prisma from '@/lib/prisma';
-
-import { TUserSignInValidator } from '@/lib/validators/user-validator';
-
+import { TUserSignInValidator, UserSignInValidator } from '@/lib/validators/user-validator';
 
 import { signIn } from '@/auth';
 
 
 
 export async function authenticateUser(data: TUserSignInValidator){
-    await signIn('credentials', data)
+    const validatedFields = UserSignInValidator.safeParse(data);
+
+    if (!validatedFields.success) {
+        return { message: 'Invalid fields!' };
+    }
+
+    const { username, password } = validatedFields.data;
+
+    await signIn('credentials', {
+        username,
+        password
+    })
 }
--- src/auth.ts
+++ src/auth.ts
@@ -1,35 +1,12 @@
 import NextAuth from 'next-auth';
-import Credentials from '@auth/core/providers/credentials';
-import * as bcrypt from 'bcrypt';
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+import authConfig from "@/auth.config";
 
 import prisma from '@/lib/prisma';
 
-
 export const { handlers: { GET, POST }, signIn, signOut, auth } = NextAuth({
-    providers: [
-        Credentials({
-            credentials: {
-                username: {},
-                password: {},
-            },
-            authorize: async (credentials) => {
-                console.log(credentials)
-                const { username, password } = credentials;
-
-                const user = await prisma.user.findFirst({
-                    where: {
-                        email: username
-                    }
-                });
-
-                if (!user) {
-                    throw new Error("User not found.")
-                }
-                return user;
-            }
-        }),
-    ],
-    pages: {
-        signIn: '/sign-in'
-    }
-})
+   adapter: PrismaAdapter(prisma),
+   session: { strategy: 'jwt' },
+   ...authConfig
+});
